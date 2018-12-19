23485c2ba8ab5349f8030489cff3c0c0729027f9
CodeElixir <14311A0431@sreenidhi.edu.in>
2018-12-19 17:47:01 +0530

    beryllium : Fix typo error. Also dont install kernel modules in system.

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -39,7 +39,7 @@ LOCAL_MODULE       := init.msm.usb.configfs.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.msm.usb.configfs.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -66,3 +66,4 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := ueventd.qcom.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
 include $(BUILD_PREBUILT)
+

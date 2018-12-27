0c242ecc9af1b0c4439027847ad7cbf8a82223f6
CodeElixir <14311A0431@sreenidhi.edu.in>
2018-12-27 20:37:24 +0530

    beryllium : Add thermal script to send required thermal message to thermal-engine.
    * This is necessary to improve perfomance. Without this, thermal-engine restricts CPU & GPU causing severe performance degradation.
      MIUI uses sysfs path /sys/class/thermal/thermal_message/sconfig to send dyanamic thermal message via PowerKeeper framework
      by calling function onPowerModeChanged(mode) & set_thermal_config_by_file in lib liboctvm_drv.so. By default the value '0'(restore mode)
      sends in idle mode and sends value '10'(evaluation mode) during Antutu benchmark , sends value '13' (Pubg mode) during pubg gameplay etc.
    * Extracted values by inspecting liboctvm_drv library. Since we dont have mechanism to send message dynamically and requires lot of work, we
      send evaluation mode by default.
    * Ship qcom.post_boot script from op6t.

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -67,3 +67,18 @@ LOCAL_SRC_FILES    := ueventd.qcom.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.thermal.sh
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := bin/init.thermal.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qcom.post_boot.sh
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := bin/init.qcom.post_boot.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_PREBUILT)
